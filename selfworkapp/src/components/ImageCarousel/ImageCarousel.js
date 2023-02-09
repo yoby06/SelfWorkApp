867b5b50fe1801db40749b1a0d8415cf8dd816d1
yoby06 <100288804+yoby06@users.noreply.github.com>
2023-02-09 16:45:51 +0300

    olacak inş

--- selfworkapp/src/components/ImageCarousel/ImageCarousel.js
+++ selfworkapp/src/components/ImageCarousel/ImageCarousel.js
@@ -1,35 +1,47 @@
-import * as React from 'react';
-import { Dimensions, Text, View } from 'react-native';
-import Carousel from 'react-native-reanimated-carousel';
+import React, { useEffect , useState} from 'react'
+import { Box,FlatList } from 'react-native';
+import {  Center, NativeBaseProvider  } from "native-base";
 
-function ImageCarousel() {
-    const width = Dimensions.get('window').width;
+
+export default function ImageCarousel() {
+    const [data, setData] = useState([]);
+    const [loading, setLoading] = useState(true);
+
+    const fetchData = async () => {
+        fetch("https://api.themoviedb.org/3/movie/popular?api_key=c57a2fc73b15c17a4e435a7d9de3be35")
+        .then(response => response.json())
+        .then(response => console.log(response))
+        setData(data);
+        setLoading(false);
+    }
+
+    const renderItem = ({ item }) => {
+        return (
+          <Box px={5} py={2} rounded="md" bg="primary.300" my={2}>
+            {item.title}
+          </Box>
+        );
+    };
+
+    useEffect(() => {
+          fetchData();
+     }, []);
+    
+
     return (
-        <View style={{ flex: 1 }}>
-            <Carousel
-                loop
-                width={width}
-                height={width / 2}
-                autoPlay={true}
-                data={[...new Array(6).keys()]}
-                scrollAnimationDuration={1000}
-                onSnapToItem={(index) => console.log('current index:', index)}
-                renderItem={({ index }) => (
-                    <View
-                        style={{
-                            flex: 1,
-                            borderWidth: 1,
-                            justifyContent: 'center',
-                        }}
-                    >
-                        <Text style={{ textAlign: 'center', fontSize: 30 }}>
-                            {index}
-                        </Text>
-                    </View>
+            <NativeBaseProvider>
+              <Center flex={1}>
+              <Box> Fetch API</Box>
+                {loading && <Box>Loading..</Box>}
+                {data && (
+                  <FlatList
+                    data={data}
+                    renderItem={renderItem}
+                    keyExtractor={(item) => item.id.toString()}
+                  />
                 )}
-            />
-        </View>
-    );
+              </Center>
+            </NativeBaseProvider>
+          );
 }
 
-export default ImageCarousel;
